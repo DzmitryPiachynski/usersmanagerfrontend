351ffc427e5c5d84c0ce03677e114d639f075794
Yahor Yanushkevich <62718988+YaYanush@users.noreply.github.com>
2023-06-29 18:09:09 +0200

    fix router

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Home from "./pages/Home";
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import AddUser from "./pages/AddUser";
+import EditUser from "./pages/EditUser";
 function App() {
   return (
       <div className="App">
@@ -12,6 +13,7 @@ function App() {
             <Routes>
                 <Route exact path="/" element={ <Home /> } />
                 <Route exact path="/add" element={ <AddUser /> } />
+                <Route exact path="/edituser/:id" element={ <EditUser /> } />
             </Routes>
         </Router>
       </div>
